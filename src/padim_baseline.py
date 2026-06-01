76c43dc7795e2ea4d70d38f34e80f6b377cf1222
algasami <105358814+algasami@users.noreply.github.com>
2026-06-01 15:55:34 +0800

    added padim metrics

--- src/padim_baseline.py
+++ src/padim_baseline.py
@@ -7,11 +7,14 @@ from timm.data.constants import IMAGENET_DEFAULT_MEAN
 from timm.data.constants import IMAGENET_DEFAULT_STD
 from torchvision.transforms.v2 import Compose, Resize, ToTensor, Normalize, CenterCrop
 
+performance_dict: dict[str, dict[str, float]] = {}
+
 def test_against_model(model, model_name, datamodule):
 	print(f"Testing {model_name} model")
 	engine = Engine()
 	engine.fit(model=model, datamodule=datamodule)
-	engine.test(model=model, datamodule=datamodule)
+	results = engine.test(model=model, datamodule=datamodule)
+	performance_dict[model_name] = dict(results[0])
 
 categories = (
 	"bottle",
@@ -30,7 +33,7 @@ categories = (
 	"wood",
 	"zipper",
 )
-dm = MVTecADPatched(
+patched_dm = MVTecADPatched(
 	root="../data/mvtec",
 	train_batch_size=32,
 	eval_batch_size=32,
@@ -44,12 +47,54 @@ dm = MVTecADPatched(
 	]),
 )
 
-adaln_resnet18_model = PadimAdaLN(backbone="resnet18", n_features=100)
-test_against_model(adaln_resnet18_model, "ResNet18 PaDiM-AdaLN", dm)
-adaln_wide_resnet50_2_model = PadimAdaLN(backbone="wide_resnet50_2", n_features=550)
-test_against_model(adaln_wide_resnet50_2_model, "Wide ResNet50-2 PaDiM-AdaLN", dm)
+# adaln_resnet18_model = PadimAdaLN(backbone="resnet18", n_features=100)
+# test_against_model(adaln_resnet18_model, "ResNet18 PaDiM-AdaLN", patched_dm)
+# adaln_wide_resnet50_2_model = PadimAdaLN(backbone="wide_resnet50_2", n_features=550)
+# test_against_model(adaln_wide_resnet50_2_model, "Wide ResNet50-2 PaDiM-AdaLN", patched_dm)
 
+# General-purpose Models
 resnet18_model = Padim(backbone="resnet18", n_features=100)
-test_against_model(resnet18_model, "ResNet18 PaDiM", dm)
+test_against_model(resnet18_model, "ResNet18 PaDiM - General", patched_dm)
 wide_resnet50_2_model = Padim(backbone="wide_resnet50_2", n_features=550)
-test_against_model(wide_resnet50_2_model, "Wide ResNet50-2 PaDiM", dm)
+test_against_model(wide_resnet50_2_model, "Wide ResNet50-2 PaDiM - General", patched_dm)
+
+# Specialized Models
+for category in categories:
+	print(f"Testing category: {category}")
+	category_dm = MVTecAD(
+		root="../data/mvtec",
+		train_batch_size=32,
+		eval_batch_size=32,
+		num_workers=8,
+		category=category,
+		augmentations=Compose([
+			Resize((256, 256)),
+			CenterCrop((256, 256)),
+			ToTensor(),
+			Normalize(mean=IMAGENET_DEFAULT_MEAN, std=IMAGENET_DEFAULT_STD, inplace=True),
+		]),
+	)
+	test_against_model(resnet18_model, f"ResNet18 PaDiM - {category}", category_dm)
+	test_against_model(wide_resnet50_2_model, f"Wide ResNet50-2 PaDiM - {category}", category_dm)
+
+headers = ["model", "image_AUROC", "image_F1Score", "pixel_AUROC", "pixel_F1Score"]
+
+# output csv file
+with open("padim_performance.csv", "w") as f:
+	f.write(",".join(headers) + "\n")
+	for model_name, performance in performance_dict.items():
+		row = [
+			model_name,
+			str(performance["image_AUROC"]),
+			str(performance["image_F1Score"]),
+			str(performance["pixel_AUROC"]),
+			str(performance["pixel_F1Score"]),
+		]
+		f.write(",".join(row) + "\n")
+
+from tabulate import tabulate
+
+print(tabulate([[model_name] + [str(performance[key]) for key in headers[1:]]
+		  for model_name, performance in performance_dict.items()],
+		  headers=headers,
+		  tablefmt="grid"))
